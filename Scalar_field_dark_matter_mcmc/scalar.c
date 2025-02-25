6ca3ce2f8b8f287623cbd016f2a4d3b9bdaa41d7
Saddam Leonardo Kap <80615413+sleonardokap@users.noreply.github.com>
2025-02-26 00:41:52 +0530

    Create scalar.c
    This is where we input our differential equation (Model).

---
+++ Scalar_field_dark_matter_mcmc/scalar.c
@@ -0,0 +1,8 @@
+// # Here, we will be supplying the differential equation which we will be executing through python. This introduction of differential equation and 
+// solving with C, boost the speed immensely. Here, like python, we will be obtaining the array corresponding to the set of differential equation. So, output will be
+// same as python.
+
+#include stdio.h
+#include stdlib.h
+#include math.h
+  
