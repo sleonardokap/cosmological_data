906cdde219bbf239ba1d6bfb0dcb61578c0f9c8b
Saddam Leonardo Kap <leokap.phy@gmail.com>
2025-07-27 23:40:46 +0800

    BAO+LCDM_equation.py
    Now incorporated the Planck likelihood.

--- BAO data/BAO+LCDM_equation.py
+++ BAO data/BAO+LCDM_equation.py
@@ -1,11 +1,15 @@
 
-# copyright Saddam Leonardo Kap 
+# copyright Saddam Leonardo Kap  
+#If you use any algorithm, please don't forget to mention the repo and cite my relevant papers. 
+# If you want to be a contributor, you can write me an email. 
+# More advanced algorithm I will publish soon. 
 
 # Tutorial link: https://youtu.be/iRYb6wQVaO8
 
 import numpy as np
 
-
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
 from scipy.interpolate import interp1d
 
 from scipy.integrate import solve_ivp
@@ -14,6 +18,8 @@ from scipy.integrate import solve_ivp
 import scipy.linalg as la
 
 import Bao
+
+import compress_planck
 
 ##############
 
@@ -57,47 +63,55 @@ def equation( z, variable, params):
     
     
     
-    od, H, dl = variable[0], variable[1], variable[2]
+    od, omm, H, dl = variable[0], variable[1], variable[2]
 
-    om0, H0 = params[0], params[1]
+    om0, H0 = params
 
-    dotH = (-3 *( wd(z, params) * od + 1))
+    orr = 1-  od - omm
+
+    wdd = -1
+
+    dotH = (-3/2 *( 4/3 * orr +  omm + (1+  wdd) * od ) )
     
     eqd = 1 / (1 + z) * (3 * od *(1 + (-1)) + dotH * od)
-    eqH = (3 / (2 * (1 + z))) * H * (-1 * od + 1)
+    eqm = 1/(1+z) * (3 * omm + 2 * omm * dotH)
+    
+    eqH = (3 / (2 * (1 + z))) * H * ( 4/3 * orr +  omm + (1+  wdd) * od )  # check these equations again. 
 
     eqdl = 1/(1+z)*dl + (1/H ) * 2.99792458e5 * (1+z)
     
-    return np.array([eqd, eqH, eqdl])
+    return np.array([eqd, eqm, eqH, eqdl])
 
 
 
-def ode_sol( params):
+def ode_sol(params):
+    om0, H0, rd, rs_val, orr0, obh = params
 
-    om0 = params[0]
-    H0 = params[1]
-    rd = params[2]
+   
 
-    od0 = 1- om0
+    od0 = 1- om0-orr0
+
+    param = np.array([om0, H0])
+
+    cmb_params = np.array([om0, H0, obh])
   
 
     
-    tmax = 1000
+    tmax = 8000
 
 
-    sol = solve_ivp(lambda t, y:equation(t,y,params), [0, 3], [od0, H0, 0], t_eval=np.linspace(0, 3, tmax), rtol=1e-2, atol=1e-2, method='RK45')
+    sol = solve_ivp(lambda t, y:equation(t,y,param), [0, 1200], [od0, om0, H0, 0], t_eval=np.linspace(0, 1200, tmax), rtol=1e-2, atol=1e-2, method='BDF')
 
 
     t_sol = sol.t
    
-    H_sol = sol.y[1]
-    dl_sol = sol.y[2]
-    
+    odsol, omsol, H_sol, dl_sol = sol.y
+        
     
 
-    H_val = interp1d(t_sol, H_sol, kind='linear', fill_value="extrapolate")
+    H_val = interp1d(t_sol, H_sol, kind='cubic')
 
-    dl_val=interp1d(t_sol, dl_sol, kind='linear', fill_value="extrapolate")
+    dl_val=interp1d(t_sol, dl_sol, kind='cubic')
 
     res_hubble = np.zeros(len(z_dataH))
 
@@ -126,19 +140,22 @@ def ode_sol( params):
 
     # BAO calculation 
 
-    chi_bao_cmb = Bao.cmb_bao(dl_val, H_val, params=params[2])
+    chi_bao_cmb = Bao.cmb_bao(dl_val, H_val, params=rd)
 
-    chi_bao_desi = Bao.desi_bao(dl_val, H_val, params=params[2])
+    chi_bao_desi = Bao.desi_bao(dl_val, H_val, params=rd)
+
+    chi_planck = compress_planck.planck_chi(dl_val, H_val, cmb_params, rs_val, zs = 1089.90)
+
+    # you can either compute zs using the paper mentioned in the planck likelihood
     
 
-    chi_tot =  chi_hubble + chi_bao_desi + chi_bao_cmb 
+    chi_tot =  chi_hubble + chi_bao_desi + chi_bao_cmb + chi_planck
         
     return chi_tot
 
 
 
-def log_prob( params):
-    
+def log_prob( params):   
     
     
     res = ode_sol(params)
@@ -150,4 +167,6 @@ def log_prob( params):
     
     
     return  res
+
+
 
